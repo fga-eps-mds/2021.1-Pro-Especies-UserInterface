be08f145099ce2714f3f0d86bf40dac0ef91865e
sudjoao <isudjoao@gmail.com>
2021-09-22 06:54:35 -0300

    Add fonts

--- pro-especies-user-interface/App.tsx
+++ pro-especies-user-interface/App.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold
+} from "@expo-google-fonts/poppins";
+
 import { Welcome } from "./src/components/Welcome";
 
 import theme from "./src/global/styles/theme";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+  if (!fontLoaded)
+    return <AppLoading />
   return (
     <ThemeProvider theme={theme}>
       <Welcome title="Olá Pró-Espécies Peixes" />
