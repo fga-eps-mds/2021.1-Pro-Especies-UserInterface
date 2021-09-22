6f8a5ad1df955b696e38a8fb7f755d899755a0cc
Natan Tavares Santana <natan.santana.dev@gmail.com>
2021-09-22 18:52:16 -0300

    #43 fix bugs

--- pro-especies-user-interface/src/components/TopBar/styles.tsx
+++ pro-especies-user-interface/src/components/TopBar/styles.tsx
@@ -1,7 +1,8 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Box = styled.View`
-    height: 57px;
+    height: ${RFValue(57)}px;
     width: 100%;
     background-color: ${({ theme }) => theme.colors.primary};
 `
--- pro-especies-user-interface/src/pages/Register/styles.tsx
+++ pro-especies-user-interface/src/pages/Register/styles.tsx
@@ -1,8 +1,8 @@
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
     flex: 1;
     align-items: center;
-    margin-top: 2rem;
     background-color: ${({ theme }) => theme.colors.background};
 `
