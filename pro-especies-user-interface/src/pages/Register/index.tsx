a889c1ef496067e5a506490b1127860b84dac0f3
Natan Tavares Santana <natan.santana.dev@gmail.com>
2021-09-23 11:48:12 -0300

    #43 Finish Register interface

--- pro-especies-user-interface/src/pages/Register/index.tsx
+++ pro-especies-user-interface/src/pages/Register/index.tsx
@@ -1,13 +1,67 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import { CityStateView, ComunityInputIcon, Container, HalfInputView, Input, InputContainer, InputView, MaterialInputIcon, RegisterButton, RegisterButtonText, RegisterButtonView, TitleContainer, TitleText } from "./styles";
 import { TopBar } from "../../components/TopBar";
-import { RegisterInput } from "../../components/RegisterInput";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export function Register() {
+    const [admin, setAdmin] = useState(false);
+
     return (
         <Container>
             <TopBar title="Cadastro" />
-            <RegisterInput icon="person-outline" placeholder="Nome" />
+            <TitleContainer>
+                <TouchableOpacity onPress={()=> {setAdmin(!admin)}}>
+                    <TitleText admin={admin}>Usuário</TitleText>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=> {setAdmin(!admin)}}>
+                    <TitleText admin={!admin}>Pesquisador</TitleText>
+                </TouchableOpacity>
+            </TitleContainer>
+            <InputContainer>
+                <InputView>
+                    <MaterialInputIcon name="person-outline"/>
+                    <Input placeholder="Nome" />
+                </InputView>
+                <InputView>
+                    <MaterialInputIcon name="mail-outline"/>
+                    <Input placeholder="Email" />
+                </InputView>
+                <InputView>
+                    <ComunityInputIcon name="phone-outline"/>
+                    <Input placeholder="Telefone" />
+                </InputView>
+                <CityStateView>
+                    <HalfInputView>
+                        <ComunityInputIcon name="compass-outline"/>
+                        <Input placeholder="Estado" />
+                    </HalfInputView>
+                    <HalfInputView>
+                        <ComunityInputIcon name="city"/>
+                        <Input placeholder="Cidade" />
+                    </HalfInputView>
+                </CityStateView>
+                <InputView>
+                    <MaterialInputIcon name="lock-outline"/>
+                    <Input placeholder="Senha" />
+                </InputView>
+                <InputView>
+                    <MaterialInputIcon name="lock-outline"/>
+                    <Input placeholder="Confirmar Senha" />
+                </InputView>
+                {
+                    admin ? (
+                        <InputView>
+                            <ComunityInputIcon name="key-outline"/>
+                            <Input placeholder="Código de Pesquisador" />
+                        </InputView>
+                    ) : null
+                }
+                <RegisterButtonView>
+                    <RegisterButton onPress={()=>{}}>
+                        <RegisterButtonText>Cadastrar</RegisterButtonText>
+                    </RegisterButton>
+                </RegisterButtonView>
+            </InputContainer>
 
         </Container>
     )
--- pro-especies-user-interface/src/pages/Register/styles.tsx
+++ pro-especies-user-interface/src/pages/Register/styles.tsx
@@ -1,8 +1,87 @@
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
 
 export const Container = styled.View`
     flex: 1;
     align-items: center;
     background-color: ${({ theme }) => theme.colors.background};
+`
+export const TitleContainer = styled.View`
+    margin-top: ${RFValue(50)}px;
+    margin-bottom: ${RFValue(20)}px;
+    flex-direction: row;
+    align-items: center;
+`
+
+export const TitleText = styled.Text<{admin: boolean}>`
+    font-family: ${({ theme }) => (p => p.admin ? theme.fonts.regular : theme.fonts.bold)};
+    font-size: ${RFValue(12)}px;
+    margin-right: ${RFValue(16)}px;
+`
+
+export const InputContainer = styled.ScrollView`
+    flex: 1;
+`
+export const InputView = styled.View`
+    align-items: center;
+    height: ${RFValue(39)}px;
+    width: ${RFValue(258)}px;
+    flex-direction: row;
+    margin-bottom: ${RFValue(32)}px;
+    border:  ${RFValue(1)}px;
+    border-radius: ${RFValue(5)}px;
+`
+
+export const Input = styled.TextInput`
+    height: ${RFValue(39)}px;
+    width: ${RFValue(258)}px;
+    flex-direction: row;
+    justify-content: flex-start;    
+    font-family: ${({ theme }) => theme.fonts.regular}
+`
+export const CityStateView = styled.View`
+    align-items: center;
+    justify-content: space-between;
+    height: ${RFValue(39)}px;
+    width: ${RFValue(258)}px;
+    flex-direction: row;
+    margin-bottom: ${RFValue(32)}px;
+`
+
+export const HalfInputView = styled.View`
+    align-items: center;
+    height: ${RFValue(39)}px;
+    width: ${RFValue(121)}px;
+    flex-direction: row;
+    border:  ${RFValue(1)}px;
+    border-radius: ${RFValue(5)}px;
+`
+    
+export const MaterialInputIcon = styled(MaterialIcons)`
+    font-size: ${RFValue(20)}px;
+    color: #C4C4C4;
+    margin: 0 ${RFValue(11)}px; 
+`
+export const ComunityInputIcon = styled(MaterialCommunityIcons)`
+    font-size: ${RFValue(20)}px;
+    color: #C4C4C4;
+    margin: 0 ${RFValue(11)}px; 
+`
+export const RegisterButtonView = styled.View`
+    align-items: center;
+`
+export const RegisterButton = styled.TouchableOpacity`
+    width: ${RFValue(156)}px;
+    height: ${RFValue(39)}px;
+    border-radius: ${RFValue(5)}px;
+    background-color: ${({ theme }) => theme.colors.secondary_dark};
+    align-items: center;
+    justify-content: center;
+`
+
+export const RegisterButtonText = styled.Text`
+    color: white;
+    font-family: ${({ theme }) => theme.fonts.bold};
+    font-size: ${RFValue(12)}px;
 `
