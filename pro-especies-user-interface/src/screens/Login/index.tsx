290c79c22616d7852727a81a9de7c5ea24739a18
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-09-29 00:24:07 -0300

    #45 Create login Page
    Co-authored-by: João Victor Batista<jvtexbat@hotmail.com>

--- pro-especies-user-interface/src/screens/Login/index.tsx
+++ pro-especies-user-interface/src/screens/Login/index.tsx
@@ -1,11 +1,119 @@
-import React from "react";
-import { Text } from "react-native";
-import { PageContainer } from "./styles";
+import React, { useState } from "react";
+import { CityStateView,
+    InputScroll,
+    ComunityInputIcon,
+    Container,
+    ErrorMessage,
+    HalfInputView,
+    Input,
+    InputBox,
+    InputContainer,
+    InputMask,
+    InputView,
+    MaterialInputIcon,
+    LoginButton,
+    LoginButtonText,
+    LoginButtonView,
+    TitleContainer,
+    TitleHighlight,
+    TitleText,
+    TouchableTitle,
+    HomeLogoContainer,
+    HomeAppImage,
+    HomeAppTitle,
+    HomeAppTitleBlue,
+} from "./styles";
+import { CreateUser } from "../../services/userServices/createUser";
+import { Alert } from "react-native";
 
-export default function Login() {
+
+
+export function Login() {
+    const [userEmailPhone, setUserEmailPhone] = useState<string|undefined>();
+    const [isEmailPhoneValid, setIsEmailPhoneValid] = useState(true);
+    const [isEmailPhoneValidMessage, setIsEmailPhoneValidMessage] = useState("");
+    const [userPhone, setUserPhone] = useState<string|undefined>();
+    const [userPassword, setUserPassword] = useState<string|undefined>();
+    const [isPasswordValid, setIsPasswordValid] = useState(true);
+    const [isPasswordValidMessage, setIsPasswordValidMessage] = useState("");
+    const [userConfirmPassword, setUserConfirmPassword] = useState<string|undefined>();
+
+    const validatePassword = (password: string) => {
+        if(userPassword !== password){
+            setIsPasswordValidMessage("Digite a mesma senha!");
+            setIsPasswordValid(false);
+        } else {
+            setIsPasswordValid(true);
+        }
+    }
+
+    // const handleLogin = async () => {
+    //     let alertMessage = "";
+    //     if(userName && userEmail && userPhone && userState && userCity && userPassword && userConfirmPassword){
+    //         if(isEmailValid && isPasswordValid && isPhoneValid){
+    //             try {
+    //                 await CreateUser(userName, userEmail, userPhone, userState, userCity, userPassword, admin, adminToken);
+    //                 alertMessage = "Conta criada com sucesso!";
+    //             } catch (error: any) {
+    //                 alertMessage = error.response.data.message;
+    //             }
+    //         } else {
+    //             alertMessage = "Preencha todos os dados corretamente!";
+    //         }
+    //     } else {
+    //         alertMessage = "Preencha todos os campos de dados para realizar o cadastro!";
+    //     }
+    //     Alert.alert(
+    //         "Cadastro",
+    //         alertMessage,
+    //         [
+    //             {
+    //                 text: "Ok",
+    //             }
+    //         ]
+    //     )
+    // }
+
+    const handlePassword = (password: string) => {
+        setUserConfirmPassword(password);
+        validatePassword(password);
+    }
+
+    const handlePhone = (phone: string) => {
+        setUserPhone(phone);
+    }
+
     return (
-        <PageContainer>
-            <Text>Login</Text>
-        </PageContainer>
+        <Container>
+            <HomeLogoContainer>
+                <HomeAppImage source={require('../../assets/logo.png')} />
+                <HomeAppTitle>
+                    Eu<HomeAppTitleBlue>Pescador</HomeAppTitleBlue>
+                </HomeAppTitle>
+            </HomeLogoContainer>
+            <InputScroll>
+                <InputContainer>
+                    <InputView>
+                        <Input placeholder="E-mail / Telefone" value={ '' } onChangeText={()=>{}} />
+                    </InputView>
+                        {/* {
+                            LoginSuccesful ? null : <ErrorMessage>{LoginErrorMessage}</ErrorMessage>
+                        } */}
+                    <InputBox/>
+                    
+                    <InputView>
+                        <MaterialInputIcon name="lock-outline"/>
+                        <Input placeholder="Senha" secureTextEntry={true} value={userPassword} onChangeText={setUserPassword} />
+                    </InputView>
+                    <InputBox/>
+                    <InputBox/>
+                    <LoginButtonView>
+                        <LoginButton>
+                            <LoginButtonText>Entrar</LoginButtonText>
+                        </LoginButton>
+                    </LoginButtonView>
+                </InputContainer>
+            </InputScroll>
+        </Container>
     )
 }
