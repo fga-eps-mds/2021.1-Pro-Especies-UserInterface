837fafb159973928494ce59316d587a2ee501e05
PhRezende-eng <ph.hr.001@gmail.com>
2021-10-09 15:30:25 -0300

    #79 Done the screen wiki, finish design screen and list items

--- pro-especies-user-interface/src/components/FishCard/styles.ts
+++ pro-especies-user-interface/src/components/FishCard/styles.ts
@@ -3,33 +3,35 @@ import { RFValue } from "react-native-responsive-fontsize";
 
 export const FishCardContaner = styled.View`
     align-items: flex-start;
-    width: ${RFValue(156,640)}px;
-    height: ${RFValue(156,640)}px;
+    width: ${RFValue(156, 640)}px;
+    height: ${RFValue(156, 640)}px;
     elevation: 8;
-    border-radius: ${RFValue(16,640)}px;
+    border-radius: ${RFValue(16, 640)}px;
     background-color: ${({ theme }) => theme.colors.background};
+    margin-bottom: 14px;
 `
 
 export const FishImage = styled.Image`
-    width: ${RFValue(156,640)}px;
-    height: ${RFValue(100,640)}px;
-    border-top-left-radius: ${RFValue(16,640)}px;
-    border-top-right-radius: ${RFValue(16,640)}px; 
+    width: ${RFValue(156, 640)}px;
+    height: ${RFValue(100, 640)}px;
+    border-top-left-radius: ${RFValue(16, 640)}px;
+    border-top-right-radius: ${RFValue(16, 640)}px; 
 `
 
 export const CommonNameText = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
-    font-size: ${RFValue(15,640)}px;
+    font-size: ${RFValue(15, 640)}px;
     line-height: 16px;
     color: ${({ theme }) => theme.colors.on_background};
-    margin-left: ${RFValue(16,640)}px;
+    margin-left: ${RFValue(16, 640)}px;
+    margin-top: 8px;
 `
 
 export const ScientificName = styled.Text`
     font-family: ${({ theme }) => theme.fonts.regular};
-    font-size: ${RFValue(12,640)}px;
+    font-size: ${RFValue(12, 640)}px;
     line-height: 13px;
     opacity: 0.3;
     color: ${({ theme }) => theme.colors.on_background};
-    margin-left: ${RFValue(16,640)}px;
+    margin-left: ${RFValue(16, 640)}px;
 `
--- pro-especies-user-interface/src/screens/Wiki/index.tsx
+++ pro-especies-user-interface/src/screens/Wiki/index.tsx
@@ -1,13 +1,13 @@
-import React, { useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { FlatList } from "react-native";
 import { FishCard, IFish } from "../../components/FishCard";
 import { TopBar } from "../../components/TopBar";
 import { getWikiFishes } from "../../services/wikiServices/getWikiFishes";
-import { PageContainer , IconFilter, SearchBarContainer, RowContainer, TouchableFilter, TextFilter,TitleContainer, TouchableTitle , TitleText , TitleHighlight, ListImages  } from "./styles";
+import { PageContainer, IconFilter, SearchBarContainer, RowContainer, TouchableFilter, TextFilter, TitleContainer, TouchableTitle, TitleText, TitleHighlight, ListImages, FishCardList, FishBodyContainer } from "./styles";
 
 
-export const  Wiki = () => {
-    
+export const Wiki = () => {
+
     const [searchQuery, setSearchQuery] = React.useState('');
     const onChangeSearch = (query: React.SetStateAction<string>) => setSearchQuery(query);
 
@@ -15,57 +15,51 @@ export const  Wiki = () => {
     const [filter, setFilter] = useState(false);
 
     const [fishes, setFishes] = useState<IFish[]>([]);
-    
+
     const updateFishes = async () => {
         const data = await getWikiFishes();
         setFishes(data);
     }
     useEffect(() => {
         updateFishes();
-    },[])
+    }, [])
 
     return (
         <PageContainer>
             <TopBar title='Biblioteca' />
-            <TitleContainer>
-                <TouchableTitle onPress={()=> {setWiki(false)}}>
-                    <TitleText wiki={wiki}>Biblioteca de Peixes</TitleText>
-                    {
-                        wiki ? null : <TitleHighlight />
-                    }
-                </TouchableTitle>
-                <TouchableTitle onPress={()=> {setWiki(true)}}>
-                    <TitleText wiki={!wiki}>Registros</TitleText>
-                    {
-                        wiki ? <TitleHighlight /> : null
-                    }
-                </TouchableTitle>
-            </TitleContainer>
-            <RowContainer>
-            <SearchBarContainer
-                placeholder="Pesquisar"
-                onChangeText={onChangeSearch}
-                value={searchQuery}
-                iconColor="#202E35"
-            />
-            <TouchableFilter onPress={() => {setFilter(false)}}>
-                <TextFilter filter={filter}>Filtro</TextFilter>
-            </TouchableFilter>
-            <IconFilter name="filter-list" />
-            </RowContainer>
-            <FlatList
-            data = {fishes}
-            // numColumns={2}
-            // initialNumToRender={2}
-            renderItem={(fish)=>(
-                <ListImages>
-                {fishes.map(fish => (
-                    <FishCard fish={fish} key={fish._id}/>
-                ))}
-                </ListImages>
-            )}
-            keyExtractor={fish => fish._id}
-            />
+            <FishBodyContainer>
+                <TitleContainer>
+                    <TouchableTitle onPress={() => { setWiki(false) }}>
+                        <TitleText wiki={wiki}>Biblioteca de Peixes</TitleText>
+                        {
+                            wiki ? null : <TitleHighlight />
+                        }
+                    </TouchableTitle>
+                    <TouchableTitle onPress={() => { setWiki(true) }}>
+                        <TitleText wiki={!wiki}>Registros</TitleText>
+                        {
+                            wiki ? <TitleHighlight /> : null
+                        }
+                    </TouchableTitle>
+                </TitleContainer>
+                <RowContainer>
+                    <SearchBarContainer
+                        placeholder="Pesquisar"
+                        onChangeText={onChangeSearch}
+                        value={searchQuery}
+                        iconColor="#202E35"
+                    />
+                    <TouchableFilter onPress={() => { setFilter(false) }}>
+                        <TextFilter filter={filter}>Filtro</TextFilter>
+                        <IconFilter name="filter-list" />
+                    </TouchableFilter>
+                </RowContainer>
+                <FishCardList
+                    data={fishes}
+                    renderItem={({ item }) => <FishCard fish={item} />}
+                    keyExtractor={item => item._id}
+                />
+            </FishBodyContainer>
         </PageContainer>
     )
 }
