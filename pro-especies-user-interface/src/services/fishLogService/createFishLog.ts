f5fe96bfb722fecd9c6679966581b284ece69e1d
Gabriel Freitas <gabriel.fbalbino@gmail.com>
2021-10-09 12:59:45 -0300

    #60 configurate to connect to API

--- pro-especies-user-interface/src/services/fishLogService/createFishLog.ts
+++ pro-especies-user-interface/src/services/fishLogService/createFishLog.ts
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import fishLogService from "./fishService"
 
 export const createFishLog = async (
@@ -10,16 +11,24 @@ export const createFishLog = async (
     lenght: number | null,
     coordenates: number | null,
 ) => {
-    const userId = "";
-    await fishLogService.post('/fishLog', {
-        userId,
-        name,
-        largeGroup,
-        group,
-        species,
-        coordenates,
-        photo,
-        length,
-        weight,
-    })
+    const _userId = AsyncStorage.getItem("@eupescador/userId");
+    const token = AsyncStorage.getItem("@eupescador/token");
+    const config = {
+        headers:{Authorization:`Bearer ${token}`}
+    }
+    await fishLogService.post(
+        '/fishLog', 
+        {
+            _userId,
+            name,
+            largeGroup,
+            group,
+            species,
+            coordenates,
+            photo,
+            length,
+            weight,
+        },
+        config
+    );
 }
