713651dcb761a2a75b63240c10a3136400a14cde
sudjoao <isudjoao@gmail.com>
2021-10-21 22:11:49 -0300

    #71 - Add mark in the map, create styles to maps
    Co-authored-by: Iuri Severo <nobreiuri@gmail.com>

--- src/screens/Maps/index.tsx
+++ src/screens/Maps/index.tsx
@@ -1,8 +1,17 @@
-import { LocationObject } from 'expo-location';
-import * as React from 'react';
-import { Map, MapContainer } from './styles';
+import React, { useState } from 'react';
+import { Map, MapButtonsView, MapContainer, MapInfoView, MapInstructions, MapInstructionsText } from './styles';
+import { Marker, MapEvent } from 'react-native-maps';
+import { TouchableOpacity, Text } from 'react-native';
+import { GreenButton } from '../../components/GreenButton';
 
 export const MyMap = ({ route }: any) => {
+    const [mark, setMark] = useState<any>({ latitude: route.params.loc.coords.latitude, longitude: route.params.loc.coords.longitude });
+
+    const handleDrag = (e: MapEvent) => {
+        console.log(e.nativeEvent.coordinate);
+        setMark(e.nativeEvent.coordinate);
+    }
+
     return (
         <MapContainer >
             <Map
@@ -12,7 +21,19 @@ export const MyMap = ({ route }: any) => {
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
-            />
+                onPress={handleDrag}
+            >
+                <Marker coordinate={mark} />
+            </Map>
+            <MapInfoView>
+                <MapInstructions>
+                    <MapInstructionsText>Clique no mapa para marcar o local onde pegou o peixe</MapInstructionsText>
+                </MapInstructions>
+                <MapButtonsView>
+                    <GreenButton text="Cancelar" buttonFunction={() => { }} />
+                    <GreenButton text="Confirmar Localização" buttonFunction={() => { }} />
+                </MapButtonsView>
+            </MapInfoView>
         </MapContainer>
     )
 }
--- src/screens/Maps/styles.ts
+++ src/screens/Maps/styles.ts
@@ -1,6 +1,8 @@
 import { Dimensions } from "react-native";
 import MapView from "react-native-maps";
+import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { GreenButton } from "../../components/GreenButton";
 
 export const MapContainer = styled.View`
     flex: 1;
@@ -12,4 +14,39 @@ export const MapContainer = styled.View`
 export const Map = styled(MapView)`
     width: ${Dimensions.get('window').width}px;
     height: ${Dimensions.get('window').height}px;
+`
+
+export const MapInfoView = styled.View`
+  flex: 1;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  padding: ${RFValue(16)}px;
+`;
+
+export const MapInstructions = styled.View`
+    width: ${RFValue(300, 640)}px;
+    height: ${RFValue(60, 640)}px;
+    margin-top: ${RFValue(40, 640)}px;
+    align-self: center;
+    justify-content: center;
+    background-color: ${({ theme }) => theme.colors.background};
+    border-radius: ${RFValue(8, 640)}px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
+
+export const MapInstructionsText = styled.Text`
+    font-family: ${({ theme }) => theme.fonts.medium};
+    font-size: ${RFValue(12, 640)}px;
+    text-align: center;
+`
+
+export const MapButtonsView = styled.View`
+    flex: 1;
+    align-items: flex-end;
+    justify-content: space-between;
+    flex-direction: row;
+`
+export const ConfirmeMapLocationBUtton = styled(GreenButton)`
+
 `
