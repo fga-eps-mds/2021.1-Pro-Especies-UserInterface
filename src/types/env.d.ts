d3532b4c77bc22a782ebe29b7adf8e14ce331d23
sudjoao <isudjoao@gmail.com>
2021-10-22 15:51:58 -0300

    Remove env file and create a config file

--- src/types/env.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-native-dotenv' {
-  export const IP_ADDRESS: string;
-  // export const ENV: 'dev' | 'prod';
-}
-
-declare module '@env' {
-  export const IP_ADDRESS: string;
-}
