23b0ce3214584856439821f366ec302e6125a7ff
sudjoao <isudjoao@gmail.com>
2021-10-27 16:09:11 -0300

    #71 - Removing height from location container

--- src/screens/Maps/styles.ts
+++ src/screens/Maps/styles.ts
@@ -51,7 +51,6 @@ export const LocationUsageInfoView = styled.View`
 `
 export const LocationUsageInfoContainer = styled.View`
     width: ${RFValue(300, 640)}px;
-    height: ${RFValue(120, 640)}px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     margin-bottom: ${RFValue(32, 640)}px;
     background-color: ${({ theme }) => theme.colors.background};
