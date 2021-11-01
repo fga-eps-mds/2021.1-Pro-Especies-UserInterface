f1d6a073a65bea018d5687b65b79c681872d210c
Daniel Coimbra <coimbrasantos.d@gmail.com>
2021-11-01 17:36:50 -0300

    #85 Changes to export Service.

--- src/services/fishLogService/exportFishLogs.ts
+++ src/services/fishLogService/exportFishLogs.ts
@@ -1,9 +1,10 @@
 import { fishLogService } from './fishService';
 
-async function ExportFishLogs(token: string) {
+async function ExportFishLogs(token: string, exportList: Array<string>) {
   const userToken = `Bearer ${token}`;
   const res = await fishLogService.get(`/fishLog/export`, {
     headers: { Authorization: userToken },
+    params: { body:  exportList },
   });
   return res.data;
 }
